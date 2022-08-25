f09176152513f299a05c6d8c9c5e1c43d13f5e9c
Yuk Fai Hsu <yukfai@Yuks-MacBook-Pro.local>
2022-08-25 11:07:36 -0400

    Add navigation through Route

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import "./navigation.styles.scss";
+
+import { ReactComponent as JasLogo } from "../../assets/jasmine.svg";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <JasLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
