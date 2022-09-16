88a6e278c02de6152bd889a51e78fbfc0edfa5f3
Yuk Fai Hsu <yukfai@Yuks-MacBook-Pro.local>
2022-09-16 19:55:05 -0400

    add navigation to different categories in Home page

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,12 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 import {
   DirectoryItemContainer,
   BackgroundImage,
   Body,
 } from "./directory-item.styles";
 
-const DirectoryItem = ({ category: { imageUrl, title } }) => {
+const DirectoryItem = ({ category: { imageUrl, title, route } }) => {
+  const navigate = useNavigate();
+
+  const onNavigateHandler = () => navigate(route);
+
   return (
-    <DirectoryItemContainer>
+    <DirectoryItemContainer onClick={onNavigateHandler}>
       <BackgroundImage imageUrl={imageUrl} />
       <Body>
         <h2>{title}</h2>
