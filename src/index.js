d70d631901185a66b31d7b081526fe8f84344a4d
Yuk Fai Hsu <yukfai@Yuks-MacBook-Pro.local>
2022-09-22 18:12:12 -0400

    perform data persistence by using redux-persist and add redux devtools

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import {PersistGate } from 'redux-persist/integration/react' 
 
 import App from "./App";
-import { store } from "./store/store";
+import { persistor, store } from "./store/store";
 
 import "./index.scss";
 
@@ -13,9 +14,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
           <App />
-      </BrowserRouter>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
