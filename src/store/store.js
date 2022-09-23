92bf202165d9b34faebe222c92b1608d42e45212
Yuk Fai Hsu <yukfai@Yuks-MacBook-Pro.local>
2022-09-23 12:19:00 -0400

    update categories to use redux-thunk and use isLoading to render spinner

--- src/store/store.js
+++ src/store/store.js
@@ -6,18 +6,19 @@ import {
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  blacklist: ["user"],
+  whitelist: ["cart"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(
   Boolean
 );
 
